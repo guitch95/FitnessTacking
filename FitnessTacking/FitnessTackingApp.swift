f202943fb19a930e86cd7068312e195f73c20e93
Guillaume Richard <guitch95@gmail.com>
2026-03-25 11:44:06 +0100

    Initial Commit

---
+++ FitnessTacking/FitnessTackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTackingApp.swift
+//  FitnessTacking
+//
+//  Created by Guillaume Richard on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
